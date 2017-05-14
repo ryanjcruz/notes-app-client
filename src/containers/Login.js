4b15984675789a12866980492e1c8bc598d5bc34
Ryan Cruz <ryanjosephcruz1@gmail.com>
2017-05-14 14:32:00 +0800

    Retrieve user token using amazon-cognito-identity.

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,6 +6,12 @@ import {
   ControlLabel,
 } from 'react-bootstrap';
 import './Login.css';
+import config from '../config.js';
+import {
+  CognitoUserPool,
+  AuthenticationDetails,
+  CognitoUser,
+} from 'amazon-cognito-identity-js';
 
 class Login extends Component {
   constructor(props) {
@@ -28,8 +34,37 @@ class Login extends Component {
       });
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
       event.preventDefault();
+
+      try {
+        const userToken = await this.login(this.state.username, this.state.password);
+        alert(userToken);
+      } catch(e) {
+        alert(e);
+      }
+  }
+
+  login(username, password) {
+    const userPool = new CognitoUserPool({
+      UserPoolId: config.cognito.USER_POOL_ID,
+      ClientId: config.cognito.APP_CLIENT_ID,
+    });
+
+    const authenticationData = {
+      Username: username,
+      Password: password,
+    };
+
+    const user = new CognitoUser({ Username: username, Pool: userPool });
+    const authenticationDetails = new AuthenticationDetails(authenticationData);
+
+    return new Promise((resolve, reject) => (
+      user.authenticateUser(authenticationDetails, {
+        onSuccess: (result) => resolve(result.getIdToken().getJwtToken()),
+        onFailure: (err) => reject(err),
+      })
+    ));
   }
 
   render() {
