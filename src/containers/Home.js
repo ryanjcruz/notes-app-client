62645195fe5fbf4cf711516d71e0b43c44a1754e
Ryan Cruz <ryanjosephcruz1@gmail.com>
2017-05-15 10:48:07 +0800

    Added dynamic display for lander or notes list depending on the userToken.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import {
+  PageHeader,
+  ListGroup,
+} from 'react-bootstrap';
 import './Home.css';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+      notes: [],
+    };
+  }
+
+  renderNotesList(notes) {
+    return null;
+  }
+
+  renderLander() {
+    return (
+      <div className="lander">
+        <h1>Scatch</h1>
+        <p>A simple note taking app</p>
+      </div>
+    );
+  }
+
+  // call render notes list when the endpoint has fetched notes
+  renderNotes() {
+    return (
+      <div className="notes">
+        <PageHeader>Your Notes</PageHeader>
+        <ListGroup>
+          { ! this.state.isLoading && this.renderNotesList(this.state.notes) }
+        </ListGroup>
+      </div>
+    );
+  }
+
+  // render the lander of the list of notes depending on the userToken
   render() {
     return (
       <div className="Home">
-          <div className="lander">
-              <h1>Scratch</h1>
-              <p>A simple note taking app</p>
-          </div>
+        { this.props.userToken === null ? this.renderLander() : this.renderNotes() }
       </div>
     );
   }
 }
 
-export default Home;
+export default withRouter(Home);
